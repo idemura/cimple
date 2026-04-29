eba796e0b147b42ee9705c0d295a95ce45edb4a9
Igor Demura <igor.demura@gmail.com>
2026-04-29 22:04:36 +0300

    AstStatement sealed

--- src/main/java/com/github/idemura/cimple/compiler/ast/AstStatement.java
+++ src/main/java/com/github/idemura/cimple/compiler/ast/AstStatement.java
@@ -1,6 +1,7 @@
 package com.github.idemura.cimple.compiler.ast;
 
-public abstract class AstStatement extends AstNode {
+public abstract sealed class AstStatement extends AstNode
+    permits AstDefer, AstExpressionStatement, AstFor, AstGoto, AstIf, AstReturn, AstVariable {
   protected AstStatement() {
     super();
   }
