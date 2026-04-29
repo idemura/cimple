20dde1a46b7e22935545e409c49ad7e16f6b6bda
Igor Demura <igor.demura@gmail.com>
2026-04-29 22:03:27 +0300

    AstExpression sealed

--- src/main/java/com/github/idemura/cimple/compiler/ast/AstExpression.java
+++ src/main/java/com/github/idemura/cimple/compiler/ast/AstExpression.java
@@ -1,6 +1,7 @@
 package com.github.idemura.cimple.compiler.ast;
 
-public abstract class AstExpression extends AstNode {
+public abstract sealed class AstExpression extends AstNode
+    permits AstApplyFunction, AstLiteral, AstNameRef {
   protected AstExpression() {}
 
   public abstract AstType getType();
