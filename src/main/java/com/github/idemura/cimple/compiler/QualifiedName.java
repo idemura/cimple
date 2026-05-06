5c2aaeb2ba2c759d62910110a39c68bdc9de2e2c
Igor Demura <igor.demura@gmail.com>
2026-05-06 09:10:36 -0700

    Comparable

--- src/main/java/com/github/idemura/cimple/compiler/QualifiedName.java
+++ src/main/java/com/github/idemura/cimple/compiler/QualifiedName.java
@@ -1,6 +1,6 @@
 package com.github.idemura.cimple.compiler;
 
-public record QualifiedName(String moduleName, String name) {
+public record QualifiedName(String moduleName, String name) implements Comparable<QualifiedName> {
   public QualifiedName(String name) {
     this(null, name);
   }
@@ -10,10 +10,29 @@ public record QualifiedName(String moduleName, String name) {
   }
 
   @Override
+  public int compareTo(QualifiedName other) {
+    var cmp = compareNullable(moduleName, other.moduleName);
+    if (cmp != 0) {
+      return cmp;
+    }
+    return name.compareTo(other.name);
+  }
+
+  @Override
   public String toString() {
     if (moduleName == null || moduleName.isEmpty()) {
       return name;
     }
     return moduleName + "::" + name;
+  }
+
+  private static int compareNullable(String left, String right) {
+    if (left == null) {
+      return right == null ? 0 : -1;
+    }
+    if (right == null) {
+      return 1;
+    }
+    return left.compareTo(right);
   }
 }
