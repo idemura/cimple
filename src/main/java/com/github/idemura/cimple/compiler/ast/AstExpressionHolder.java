add3c6bceac42851e58e09c9036ddef8f1c94432
Igor Demura <igor.demura@gmail.com>
2026-05-12 07:35:43 +0300

    Comment

--- src/main/java/com/github/idemura/cimple/compiler/ast/AstExpressionHolder.java
+++ src/main/java/com/github/idemura/cimple/compiler/ast/AstExpressionHolder.java
@@ -2,6 +2,8 @@ package com.github.idemura.cimple.compiler.ast;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+// Marks the ownership boundary for an expression tree. Rewrites replace the holder root, so
+// statements and declarations do not need custom code for each expression field.
 public class AstExpressionHolder extends AstNode {
   private AstExpression root;
 
